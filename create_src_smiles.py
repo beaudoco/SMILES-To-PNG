4c4c7b29ed78e4af64e2b213e86178484cea432a
Collin Beaudoin <beaudoco@mail.gvsu.edu>
2021-10-20 20:27:41 -0700

    splitting out reactions for now

--- create_src_smiles.py
+++ create_src_smiles.py
@@ -4,11 +4,11 @@ content = smiles.read()
 chunks = content.split('\n')
 chunks.remove('')
 for idx in range(len(chunks)):
-    chunks[idx] = chunks[idx].replace(" ", "")
+    chunks[idx] = chunks[idx].replace(" ", "").split('>',1)[1]
 smiles.close()
 mols = [pybel.readstring("smi", x) for x in chunks]
 for idx in range(len(mols)):
-    mols[idx].draw(False, "USPTO-50K-IMAGES-/mol-{0}.png".format(idx))
+    mols[idx].draw(False, "USPTO-50K-IMAGES/mol-{0}.png".format(idx))
 # fps = [x.calcfp() for x in mols]
 # print(fps[0].bits, fps[1].bits) 
 # print(fps[0] | fps[1])
